030fd3e62df1a508c07274eee8f5e4832e45e2da
Sophie Luna Schumann <me@luna.vg>
2021-03-23 11:12:14 +0100

    added argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
 mod lib;
+use std::error::Error;
+
 use lib::Pak;
 
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    let pak = Pak::from_file("extras.pak".to_string()).unwrap();
-    println!("Pak file found: {}", pak);
 
-    println!("Listing all files in {}:", pak.pak_path);
-    pak.files.iter().for_each(|i| println!("\t{}", i.name));
+    let matches = App::new("Pak")
+        .version("0.1")
+        .author("Sophie Luna Schumann <me@sophie.lgbt>")
+        .about("Quake/Half-Life Pak file manipulation utility")
+        
+        .subcommand(SubCommand::with_name("list")
+            .about("List files inside .pak")
+            .arg(Arg::with_name("path")
+                .help("Path to .pak file")
+                .index(1)
+                .required(true))
+        )
+        .get_matches();
 
-    let test_file = &pak.files[0];
-    println!(
-        "Trying to save first file ({}) to 'test.bin'...",
-        test_file.name
-    );
-    test_file.save_to("test.bin".to_string()).unwrap();
-    println!("If it didn't crash, it succeeded! Yaaay!!! 🌈🦄🦊");
+    if let Some(matches) = matches.subcommand_matches("list") {
+        let path = matches.value_of("path").unwrap();
+        match list_pak_file(path) {
+            Ok(_) => {}
+            Err(e) => {eprintln!("Pak file error: {}", e)}
+        }
+    }
+}
+
+fn list_pak_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let pak = Pak::from_file(path.to_string())?;
+    //println!("Pak file found: {}", pak);
+    //println!("Listing all files in {}:", pak.pak_path);
+    pak.files.iter().for_each(|i| println!("{}", i.name));
+    Ok(())
 }
